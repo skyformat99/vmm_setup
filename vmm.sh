abb53fce563afae197346ddff767325c16b7e8d8
gonzalo <gonzalo@x61.com.ar>
2017-07-08 01:21:02 -0300

    add some nice message

--- vmm.sh
+++ vmm.sh
@@ -18,6 +18,10 @@ echo "[+] Installing files..."
 echo ""
 install -m 0640 -g wheel -o root /tmp/hostname.vether0 /etc
 install -m 0600 -g wheel -o root /tmp/{pf,sysctl,dhcpd,vm}.conf /etc
+rm /tmp/hostname.vether0 /tmp/*.conf
+echo ""
+echo "[+] Downloading OpenBSD kernels and Alpine Linux iso..."
+echo ""
 mkdir -p /VMs/sets/snapshots/amd64/
 mkdir -p /VMs/linux
 cd /VMs/linux && ftp -V https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-virt-3.6.0-x86_64.iso
